1b0e3126b066f85ab4b5d2c5627802a2b1bd6590
Huynh Nga <41855929+ngahohuynh@users.noreply.github.com>
2019-04-19 13:03:19 +0700

    Create database.py

---
+++ common/database.py
@@ -0,0 +1,23 @@
+import pymongo
+
+class Database(object):
+    #static variables
+    URI = "mongodb://127.0.0.1:27017"
+    DATABASE = None
+
+    @staticmethod
+    def initialize():
+        client = pymongo.MongoClient(Database.URI)
+        Database.DATABASE = client['buildpywebapps']
+
+    @staticmethod
+    def insert(collection, data):
+        Database.DATABASE[collection].insert(data)
+
+    @staticmethod
+    def find(collection, query):
+        return Database.DATABASE[collection].find(query)
+
+    @staticmethod
+    def find_one(collection, query):
+        return Database.DATABASE[collection].find_one(query)
